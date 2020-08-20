9a35ae344b8ccdaaed159aada0fe98c00742b268
darsh <darsh@DESKTOP-B3EVEU1>
2020-08-20 20:43:20 +0530

    this is for reference.

---
+++ src/explainingGitHub/Practice.java
@@ -0,0 +1,11 @@
+package explainingGitHub;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello friends");
+
+	}
+
+}
