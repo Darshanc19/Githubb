8387ad495afb0cd54cd65596f96fad235fbc13a3
darsh <darsh@DESKTOP-B3EVEU1>
2020-08-20 21:27:44 +0530

    second update

--- src/explainingGitHub/Practice.java
+++ src/explainingGitHub/Practice.java
@@ -6,6 +6,7 @@ public class Practice {
 		// TODO Auto-generated method stub
 		System.out.println("hello friends");
 		System.out.println("how are you??");
+		System.out.println("lets hope every thing works well");
 
 	}
 
